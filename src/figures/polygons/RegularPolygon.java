7cf3f63bbe0976c050faa155a7c06c119169e961
Sipeyko_D <MintaiDS@ya.ru>
2016-03-08 17:09:17 +0200

    Regular Polygones count points function (hasn't been tested)

--- src/figures/polygons/RegularPolygon.java
+++ src/figures/polygons/RegularPolygon.java
@@ -10,7 +10,19 @@ public class RegularPolygon extends Polygon {
         ArrayList<Point> res = new ArrayList<>();
         Point fPoint = arr.get(0);
         Point sPoint = arr.get(1);
-        int dist = (int) fPoint.distance(sPoint);
+        int x = sPoint.x - fPoint.x;
+        int y = sPoint.y - fPoint.y;
+        int x1,y1;
+        double alpha = Math.PI*2./number;
         res.add(sPoint);
+        for (int i = 0; i < number; ++i){
+            x1 = x;
+            y1 = y;
+            x = (int) (Math.cos(alpha)*x1 +Math.sin(alpha)*y1);
+            y = (int) (Math.cos(alpha)*y1 +Math.sin(alpha)*x1);
+            res.add(new Point(x + fPoint.x ,y + fPoint.y));
+        }
+        setPoints(res);
+        setPointNumber(res.size());
     }
 }
